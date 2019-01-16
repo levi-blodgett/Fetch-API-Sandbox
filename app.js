fb00cc8cd29eeaae31a1417a357e6accab800b67
Levi <levi_blodgett@outlook.com>
2019-01-15 20:09:35 -0600

    Learned about Fetch API, using json, text, and external API

---
+++ app.js
@@ -0,0 +1,62 @@
+document.getElementById('button1').addEventListener('click', getText);
+
+document.getElementById('button2').addEventListener('click', getJson);
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// Get local text file data
+function getText() {
+  // Fetch returns promises, which you have to use .then with
+  fetch('test.txt')
+    .then(function(res) {
+      // console.log(res); // Prints a response
+      // console.log(res.text()); // Prints a promise
+      return res.text(); // Returns a promise
+      // res.text() is just that way because it is a text file, it would be res.JSON() if it were a JSON file
+    })
+    .then(function(data) {
+      console.log(data); // Returns the data, in this case the text inside the txt file
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get local json data
+function getJson() {
+  fetch('posts.json')
+    .then(function(res) {
+      return res.json(); 
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`
+      });
+      document.getElementById('output').innerHTML = output
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get local json data
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(function(res) {
+      return res.json(); 
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`
+      });
+      document.getElementById('output').innerHTML = output
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
